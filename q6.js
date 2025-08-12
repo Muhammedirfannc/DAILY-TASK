674ce9685fabe2acc55a8d286e88d5d2df11a994
Muhammed irfan <muhammedirfannc7@gmail.com>
2025-08-12 12:20:56 +0530

    div by 3 & unique value

---
+++ q6.js
@@ -0,0 +1,15 @@
+//1
+
+function a(x){
+    if(x%3==0 && x%5==0){
+        console.log("FIZZBUZZ")
+}else if(x%3==0){
+    console.log("BUZZ")
+   
+}else if(x%3!==0){
+    console.log("FIZZ")
+}
+
+};
+a(1);
+
