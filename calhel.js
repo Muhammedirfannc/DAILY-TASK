a2591a8be39aec6dd491545484a8d0853fe01c66
Muhammed irfan <muhammedirfannc7@gmail.com>
2025-08-29 18:01:27 +0530

    div by 3 & unique value

---
+++ calhel.js
@@ -0,0 +1,44 @@
+
+function loadDashBoard(id,callback){
+
+   let error;
+   setTimeout(()=>{
+      if(error)
+      {
+         return callback(error);
+      }
+      console.log("Fetching user data....");
+
+      setTimeout(()=>{
+         if(error)
+      {
+         return callback(error);
+      }
+         console.log("Fetching user friend list...");
+
+         setTimeout(()=>{
+            error ="post Fetching error ";
+              if(error)
+            {
+                return callback(error);
+            }
+            console.log("Fetching uer posts...")
+
+            setTimeout(()=>{
+                 if(error)
+                  {
+                    return callback(error);
+                  }
+               console.log("fetching user comments....");
+               
+            },2000)
+         },2000);
+      },2000);
+   },2000);
+}
+
+function errorHandle(error){
+   console.log("Error has occured")
+}
+
+loadDashBoard("101",errorHandle)
